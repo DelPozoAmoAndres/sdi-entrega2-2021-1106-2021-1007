2938d8286aff701a33fa6cc321fc68f004eb67c0
DelPozoAmoAndres <asdfandres09@gmail.com>
2021-04-26 23:48:18 +0200

    Creación de estructura

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
 let express = require('express');
 let app = express();
-let os = require('os');
+
+let swig = require('swig');
+
+// Configuración de mongodb
+let mongo = require('mongodb');
+app.set('port', 8081);
+app.set('db', 'mongodb+srv://admin:admin@cluster0.mssmg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app, mongo);
+
+//Rutas/controladores por lógica
+require("./routes/rusuarios.js")(app, swig, gestorBD);  // (app, param1, param2, etc.)
+
 let puerto = 3000;
 
 app.listen(puerto, function() {
