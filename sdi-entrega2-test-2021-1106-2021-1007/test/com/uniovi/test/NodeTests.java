9e84f51e91f71e3ab59aa86b8dca43f50c0b20e6
Sergio <uo271563@uniovi.es>
2021-04-29 17:20:58 +0200

    Cambio de estructura del repositorio y creacion del proyecto de tests

---
+++ sdi-entrega2-test-2021-1106-2021-1007/test/com/uniovi/test/NodeTests.java
@@ -0,0 +1,14 @@
+package com.uniovi.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NodeTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
