dfeff137bd93ed3cde87c7d4de4e770fba882249
Andres <asdfandres09@gmail.com>
2021-05-09 20:33:41 +0200

    Listado de productos por api

--- sdi-entrega2-2021-1106-2021-1007/rutas/rapiproductos.js
+++ sdi-entrega2-2021-1106-2021-1007/rutas/rapiproductos.js
@@ -1,35 +1,49 @@
-module.exports = function (app, gestorBD){
-    app.post("/api/autenticar", function (req, res){
+module.exports = function (app, gestorBD) {
+    app.post("/api/autenticar", function (req, res) {
         let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
             .update(req.body.password).digest('hex');
 
         let criterio = {
-            email : req.body.email,
-            password : seguro
+            email: req.body.email,
+            password: seguro
         }
-        if (criterio.email==undefined || criterio.email.length==0 || seguro.length==0){
+        if (criterio.email == undefined || criterio.email.length == 0 || seguro.length == 0) {
             res.status(500);
             res.json({
-                error : "se ha producido un error de validacion"
+                error: "se ha producido un error de validacion"
             })
             return;
         }
 
-        gestorBD.obtenerUsuarios(criterio, function (usuarios){
-            if (usuarios==null || usuarios.length==0) {
+        gestorBD.obtenerUsuarios(criterio, function (usuarios) {
+            if (usuarios == null || usuarios.length == 0) {
                 res.status(40); //Unauthorized
                 res.json({
-                    autenticado : false
+                    autenticado: false
                 })
             } else {
                 let token = app.get('jwt').sign(
-                    {usuario: criterio.email , tiempo: Date.now()/1000},
+                    {usuario: criterio.email, tiempo: Date.now() / 1000},
                     "secreto");
                 res.status(200);
                 res.json({
-                    autenticado : true,
-                    token : token
+                    autenticado: true,
+                    token: token
                 })
+            }
+        });
+    });
+    app.get("/api/producto", function (req, res) {
+        let criterio = {"autor": {$ne: req.session.usuario}}
+        gestorBD.obtenerProductos(criterio, function (productos) {
+            if (productos == null) {
+                res.status(500);
+                res.json({
+                    error: "se ha producido un error"
+                })
+            } else {
+                res.status(200);
+                res.send(JSON.stringify(productos));
             }
         });
     });
